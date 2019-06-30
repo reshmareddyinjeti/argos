69bb6638e568dffb08f84a6382c32f99c95e539a
sandeep <sandeep@sandeeps-MacBook-Pro.local>
2019-06-30 14:30:30 +0100

    added new file

---
+++ Argos.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Argos extends  Nike{
+    public void menswear(){
+        System.out.println("shirts");
+        System.out.println("shoes");
+        System.out.println("hoodie");
+    }
+    public void womenwear(){
+        System.out.println("shirts");
+        System.out.println("maxy");
+        System.out.println("shoes");
+        System.out.println("hoodie");
+    }
+}
